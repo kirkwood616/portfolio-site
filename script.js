3861f18c21db75f33c7bb38455c7eaf4d911d9c8
kirkwood616 <kirkwood616@gmail.com>
2022-05-20 18:45:59 -0400

    Thumbnail changed to animation, refactoring

--- script.js
+++ script.js
@@ -8,19 +8,23 @@ projects.forEach((project) => {
   const card = document.createElement("div");
   card.classList.add("card");
   container.appendChild(card);
+
   // THUMBNAIL
   const thumbnail = document.createElement("div");
   thumbnail.classList.add("thumbnail");
   card.appendChild(thumbnail);
+
   // THUMBNAIL TEXT
   const thumbText = document.createElement("span");
   thumbText.classList.add("thumbText");
   thumbText.innerText = project.thumbText;
   thumbnail.appendChild(thumbText);
+
   // THUMBNAIL SHADOW
   const thumbnailShadow = document.createElement("div");
   thumbnailShadow.classList.add("thumbnailShadow");
   thumbnail.appendChild(thumbnailShadow);
+
   // THUMBNAIL IMAGE
   const image1 = document.createElement("img");
   image1.classList.add("thumbnailImage");
@@ -28,29 +32,28 @@ projects.forEach((project) => {
   image1.src = `./${project.image1}`;
   image1.setAttribute("alt", project.name);
   thumbnail.appendChild(image1);
-  // card.addEventListener("mouseenter", () => {
-  //   image1.src = `./${project.image2}`;
-  // });
-  // card.addEventListener("mouseleave", () => {
-  //   image1.src = `./${project.image1}`;
-  // });
+
   // NAME
   const h3 = document.createElement("h3");
   h3.innerText = project.name;
   card.appendChild(h3);
+
   // DESCRIPTION
   const p = document.createElement("p");
   p.innerText = project.description;
   card.appendChild(p);
+
   // BUILD TOOLS
   const builtWith = document.createElement("p");
   builtWith.classList.add("builtWith");
   builtWith.innerText = "Build Tools:";
   card.appendChild(builtWith);
+
   // PIPE SEPARATOR
   const pipe = document.createElement("span");
   pipe.classList.add("pipe");
   pipe.innerText = " | ";
+
   // BUILD TOOLS
   const buildTools = document.createElement("p");
   buildTools.classList.add("buildTools");
@@ -63,10 +66,12 @@ projects.forEach((project) => {
     buildTools.appendChild(toolText);
     if (index !== project.buildTools.length - 1) buildTools.appendChild(pipe);
   });
+
   // LINKS
   const links = document.createElement("p");
   links.classList.add("links");
   card.appendChild(links);
+
   // LINK => LIVE SITE
   const liveSite = document.createElement("a");
   liveSite.href = project.liveSite;
@@ -74,6 +79,7 @@ projects.forEach((project) => {
   liveSite.innerText = "Live Site";
   links.appendChild(liveSite);
   links.appendChild(pipe);
+
   // LINK => SOURCE CODE
   const sourceCode = document.createElement("a");
   sourceCode.href = project.sourceCode;
